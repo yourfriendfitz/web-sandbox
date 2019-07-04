473c990a6668f7390b224d3f8907daf0e64d8557
J. Fitzgerald <48025555+yourfriendfitz@users.noreply.github.com>
2019-07-04 16:11:38 -0500

    Update exec.js

--- node-sandbox/exec.js
+++ node-sandbox/exec.js
@@ -1,1 +1,14 @@
+const cp = require("child_process");
 
+cp.exec("open http://www.google.com");
+
+cp. exec("open --a Terminal")
+
+cp.exec("ls", (err, data, stderr) => {
+  if (err) {
+    throw err
+  } else if (stderr) {
+    throw stderr
+  }
+  console.log(data);
+});
