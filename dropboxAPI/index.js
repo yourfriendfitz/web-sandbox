c607a6d6884d4d8f254a8d20f59f9b305a78a3c5
J. Fitzgerald <48025555+yourfriendfitz@users.noreply.github.com>
2019-07-05 10:22:46 -0500

    Update index.js

--- dropboxAPI/index.js
+++ dropboxAPI/index.js
@@ -1,6 +1,13 @@
-import { Dropbox } from "dropbox"
+import { Dropbox } from "dropbox";
 
 const dbx = new Dropbox({
-    accessToken: "6Bo0DpVZ7VAAAAAAAAAAQHHTZLUaBrTrPnj9xEpXKTZdsjL47k3ZOMkJYax0dZsj",
-    fetch
-})
+  accessToken:
+    "6Bo0DpVZ7VAAAAAAAAAAQHHTZLUaBrTrPnj9xEpXKTZdsjL47k3ZOMkJYax0dZsj",
+  fetch
+});
+
+dbx
+  .filesListFolder({
+    path: ""
+  })
+  .then(res => console.log(res));
