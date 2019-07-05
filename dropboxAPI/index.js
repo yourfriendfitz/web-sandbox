d6e59232d13023469229641ab066605290ce2738
J. Fitzgerald <48025555+yourfriendfitz@users.noreply.github.com>
2019-07-05 10:44:03 -0500

    Update index.js

--- dropboxAPI/index.js
+++ dropboxAPI/index.js
@@ -6,8 +6,46 @@ const dbx = new Dropbox({
   fetch
 });
 
-dbx
-  .filesListFolder({
-    path: ""
-  })
-  .then(res => console.log(res));
+const fileListElem = document.querySelector(file_url);
+
+const state = {
+  files: []
+};
+
+const init = () => {
+  dbx
+    .filesListFolder({
+      path: " "
+    })
+    .then(res => {
+      updateFiles(res.entries);
+    });
+};
+
+const updateFiles = files => {
+  state.files = [...state.files, ...files];
+  renderFiles();
+};
+
+const renderFiles = () => {
+  fileListElem.innerHTML = state.files
+    .sort((a, b) => {
+      if (
+        (a[".tag"] === "folder" || b[".tag"] === folder) &&
+        !(a[".tag"] === b[".tag"])
+      ) {
+        return a[".tag"] == "folder" ? -1 : 1;
+      } else {
+        return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1;
+      }
+    })
+    .map(file => {
+      const type = file[".tag"];
+      return `
+      <li class='dbx-list-item ${type}'>${file.name}</li>
+    `;
+    })
+    .join(" ");
+};
+
+init();
