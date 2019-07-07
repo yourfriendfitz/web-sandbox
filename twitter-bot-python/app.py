afbff70a944d2ccb9ef4a4e2e087e3358d9b9a86
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-07 12:39:58 -0500

    still not opening a chrome window

--- twitter-bot-python/app.py
+++ twitter-bot-python/app.py
@@ -1,20 +1,34 @@
-#!path+=/Users/fitz/Library/Python/3.7/bin
+#!path+=**/Python/3.7/bin:Google\ Chrome.app/Contents/MacOS
+# Please download
+# geckodriver from, This is a macos download link straight to the zip
+#  https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-macos.tar.gz
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import selenium.webdriver.chrome.service as service
 
 import time
 
-class TwitterBot:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-        self.bot = webdriver.Firefox()
 
-    def login(self):
+class GoogleBot:
+    def __init__(self, search_item):
+        self.bot = service.Service(
+            '**/Applications/Google\ Chrome.app/Contents/MacOS')
+        self.search = search_item
+
+    def search(self):
+        search_input = self.search
         bot = self.bot
-        bot.get("https://twitter.com")
-        time.sleep(3)
+        bot.start()
+        capabilities = {
+            'chrome.binary': '**/Google\ Chrome\ Canary.app/Contents/MacOS'}
+        driver = webdriver.Remote(service.service_url, capabilities)
+        driver.get('http://www.google.com/xhtml')
+        time.sleep(5)
+        # searchbar = bot.find_element_by_class_name("gLFyfgsfi")
+        # searchbar.clear()
+        # searchbar.send_keys(search_input)
 
-ed = TwitterBot("make", "make")
-ed.login()
+
+gs = GoogleBot("NBA")
+gs.search()
 
