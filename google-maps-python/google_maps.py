ba291c387d90d2433031a46b00057897198628e6
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-06 21:20:49 -0500

    showing Python's functionality with Google Maps API

---
+++ google-maps-python/google_maps.py
@@ -0,0 +1,27 @@
+import googlemaps
+from datetime import datetime
+
+gmaps = googlemaps.Client(key='AIzaSyBacmzQ-YkM-3wy2cE0OCJbnDt8X6Zim1c')
+
+# Geocoding an address
+# geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+# # Look up an address with reverse geocoding
+# reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions("H-E-B, 10251 Kempwood Dr, Houston, TX 77043",
+                                     "Whataburger, 2429 Gessner Rd, Houston, TX 77080",
+                                     mode="walking",
+                                     departure_time=now)
+
+html_instructions = []                                    
+
+
+for array in directions_result:
+    for item in array["legs"]:
+        for step in item["steps"]:
+            html_instructions.append(step["html_instructions"])
+
+print(html_instructions)
