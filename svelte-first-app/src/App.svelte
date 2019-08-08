ed25d1a57a9809866643e6c3e1a1709b2d1b7e29
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-08 10:07:19 -0500

    first try with svelte

---
+++ svelte-first-app/src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  const fetcher = async () => {
+    const api = await fetch(
+      "https://secret-headland-59005.herokuapp.com/api/movies"
+    );
+    return api.json();
+  };
+  export let name;
+  export let data = fetcher();
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+  li {
+	  list-style: none;
+  }
+</style>
+
+{#await data}
+  <p>...waiting</p>
+{:then array}
+  <p>got the data</p>
+  {#each array as { title, id }}
+    <li>{title} - {id}</li>
+  {/each}
+{/await}
